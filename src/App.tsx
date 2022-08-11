56aad7179c9a72b0bbbe72e4e1e7d8ae2e6d65dc
Svetlana Sheiko <svetlanka.sheyko@gmail.com>
2022-08-11 14:21:34 +0300

    default state btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { title } from 'process';
+// import { title } from 'process';
 import React from 'react';
+
 //@ts-ignore
-import styles from '../App.module.css';
-const Button = () =>{
+import styles from './App.module.css';
 
-  const onClick = () => {}
-  const tile = ''
-  return <div onClick={onClick} className={styles.button}>{title}</div>
-}
 
-function App() {
+ const Button = ({title,onClick,type,className}:any) =>{
+  
+//   // const {title,onClick} = props;
+//   // const onClick = () => {}
+//   // const title = ''
+   return <div onClick={onClick}  className={`${styles.button} ${className} `}>{title}</div>
+ }
+
+
+export const  App = () => {
   return (
     <div className={styles.app}>
-     <Button/>
+     <Button title ={'Primary'} type = "primary" onClick = {()=> alert('Done')}/>
+     <Button title ={'Secondary'} type = "secondary" onClick = {()=> alert('Done') }className ={styles.greybtn}  />
+     <Button title ={'Secondary 2'}  type = "thirdly "onClick = {()=> alert('Done')} className ={styles.whitebtn} />
     </div>
   );
 }
